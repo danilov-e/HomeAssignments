15c853ecdb7e6e6a5d9675bff5a43f9563d8bbb0
Egor Danilov <st141853@student.spbu.ru>
2025-10-18 16:21:18 +0300

    Add realisation of functions

---
+++ Assignment2/b/countValue.cpp
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <string>
+#include <iostream>
+
+int countValue() {
+	std::string input;
+	char tempChar;
+
+	std::cout << "Enter a mathematical expression in Reverse Polish Notation: " << std::endl;
+	std::getline(std::cin, input);
+	const int size_of_input = input.length();
+	int* stack = new int[size_of_input];
+	int top = -1;
+
+	for (int i = 0; i < size_of_input; i++) {
+		tempChar = input[i];
+		if (tempChar == ' ') {continue;}
+
+		if (tempChar >= '0' && tempChar <= '9') {
+			int number = 0;
+			while (i < size_of_input && input[i] >= '0' && input[i] <= '9') {
+				tempChar = input[i];
+				number = number * 10;
+				number = number + (tempChar - '0');
+				i += 1;
+			}
+			top += 1;
+			stack[top] = number; //put number in the stack
+		}
+		else {
+			if (tempChar == '+') {stack[top-1] = stack[top-1] + stack[top];}
+			if (tempChar == '-') {stack[top-1] = stack[top-1] - stack[top];}
+			if (tempChar == '*') {stack[top-1] = stack[top-1] * stack[top];}
+			if (tempChar == '/') {stack[top-1] = stack[top-1] / stack[top];}
+			top -= 1;
+		}
+	}
+
+	int value = stack[top];
+	delete[] stack;
+	std::cout << value << std::endl;
+	return value;
+}
