d665410a8145432876fb3f44af4e6afc21bedb37
Egor Danilov <st141853@student.spbu.ru>
2025-12-12 14:35:03 +0300

    Add rational class

---
+++ Assignment5/rational.h
@@ -0,0 +1,28 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+#include <iostream>
+
+class Rational {
+private:
+    long num;
+    long den;
+
+    void normalize();
+
+public:
+    Rational(long n = 0, long d = 1);
+
+    long numerator() const { return num; }
+    long denominator() const { return den; }
+
+    Rational operator+(const Rational& other) const;
+    Rational operator-(const Rational& other) const;
+    Rational operator*(const Rational& other) const;
+    Rational operator/(const Rational& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Rational& r);
+    friend std::istream& operator>>(std::istream& is, Rational& r);
+};
+
+#endif // RATIONAL_H
